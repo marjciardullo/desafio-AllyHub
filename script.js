d2b134d856eb3c9e43329bd33230774f218bf515
Marjorye Ciardullo <82274021+marjciardullo@users.noreply.github.com>
2021-06-21 23:52:35 -0300

    commit

---
+++ script.js
@@ -0,0 +1,95 @@
+class courses {
+  constructor(course, description, duraction, Sdate, Edate) {
+    this.course = course;
+    this.description = description;
+    this.duraction = duraction;
+    this.Sdate = Sdate;
+    this.Edate = Edate;
+  }
+}
+
+class ui {
+  static displayCourses() {
+    const someCourses = Store.getCourses();
+
+    someCourses.forEach((item) => ui.addCourse(item));
+  }
+
+  static addCourse(vector) {
+    const list = document.querySelector("#courses-list");
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <td>${vector.course}</td>
+    <td>${vector.description}</td>
+    <td>${vector.duraction}</td>
+    <td>${vector.Sdate}</td>
+    <td>${vector.Edate}</td>
+    `;
+    list.appendChild(row);
+  }
+
+  static clearField() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#Sdate").value = "";
+    document.querySelector("#Edate").value = "";
+    document.querySelector("#duraction").value = "";
+    document.querySelector("#description").value = "";
+  }
+}
+
+class Store {
+   static getCourses () {
+    let cursos;
+    if(localStorage.getItem('cursos') === null){
+        cursos = [];
+    } else {
+        cursos = JSON.parse(localStorage.getItem('cursos'));
+    }
+    return cursos
+    }
+
+   static addCourses (course) {
+    const cursos = Store.getCourses();
+    cursos.push(course)
+    localStorage.setItem('cursos', JSON.stringify(cursos));
+
+
+    }
+    
+}
+
+document.addEventListener("DOMContentLoaded", ui.displayCourses);
+
+document.querySelector("#course-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const course = document.querySelector("#title").value;
+  const startDate = document.querySelector("#Sdate").value;
+  const endDate = document.querySelector("#Edate").value;
+  const duraction = document.querySelector("#duraction").value;
+  const description = document.querySelector("#description").value;
+
+  if (
+    course === "" ||
+    startDate === "" ||
+    endDate === "" ||
+    duraction === "" ||
+    description === ""
+  ) {
+    alert("Por favor, preencha todos os campos!");
+  } else {
+    const nameCourse = new courses(
+      course,
+      startDate,
+      endDate,
+      duraction,
+      description
+    );
+
+    ui.addCourse(nameCourse);
+
+    Store.addCourses(nameCourse);
+
+    ui.clearField();
+  }
+});
